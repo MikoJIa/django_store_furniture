e2545a23a9f471289410898a04ebb19d61a78ae0
MikoJIa <Kolya_popov_86@tut.by>
2024-01-04 15:17:50 +0300

    create base.html, about.html

---
+++ app_forniture/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from main import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about-us/', views.about, name='about'),
+]
