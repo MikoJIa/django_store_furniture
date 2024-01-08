33597b4b2ff36a243656ac9708390da878f2f159
MikoJIa <Kolya_popov_86@tut.by>
2024-01-08 19:10:45 +0300

    add debug-toolbar

--- app_forniture/app_forniture/settings.py
+++ app_forniture/app_forniture/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "debug_toolbar",
+
     "main.apps.MainConfig",
     "goods.apps.GoodsConfig"
 ]
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "app_forniture.urls"
@@ -122,6 +126,10 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 # Default primary key field type
